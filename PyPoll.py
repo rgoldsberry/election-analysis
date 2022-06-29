5c2b4c63bb99e85b9260c5e0700de6c64b7f2091
rgoldsberry <107135474+rgoldsberry@users.noreply.github.com>
2022-06-29 14:45:32 -0600

    Adding election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -9,14 +9,65 @@
 import csv
 import os
 
+# variables we'll need with the file
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
 #build path to and open the election results
 file_to_load = os.path.join("resources", "election_results.csv")
 
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-
+    #Get header row out of data
     headers = next(file_reader)
-    print(headers)
+
+    for row in file_reader:
+        #running tally of votes
+        total_votes += 1
+        
+        #building unique list of candidates
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        
+        #running tally of candidate votes
+        candidate_votes[candidate_name] += 1
+
+
+#winning candidate variables
+winning_count = 0
+winning_percentage = 0
+winning_candidate = ""
+
+
+#loop through output and determine vote totals and winne
+for candidate in candidate_options:
+    
+    #determine and print each candidates vote total/percentage
+    votes = candidate_votes[candidate]
+    candidate_percent = float(votes)/float(total_votes)*100
+
+    #print all candidate votes
+    print(f'{candidate}: {candidate_percent:.1f}% ({votes:,})\n')
+
+    #determine the winner
+    if (votes > winning_count) and (candidate_percent > winning_percentage):
+        winning_count = votes
+        winning_percentage = candidate_percent
+        winning_candidate = candidate
+
+#print out the winner
+winning_candidate_summary = (
+    f'-------------------------\n'
+    f'Winner: {winning_candidate}\n'
+    f'Winning Vote Count: {winning_count:,}\n'
+    f'Winning Percentage: {winning_percentage:.1f}%\n'
+    f'-------------------------\n'
+)
+
+print(winning_candidate_summary)
 
 
 
