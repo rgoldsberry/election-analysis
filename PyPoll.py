d1cc0a41308359e0bcc4a1dd7dcb0f5363402ef2
rgoldsberry <107135474+rgoldsberry@users.noreply.github.com>
2022-06-29 16:35:23 -0600

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -9,7 +9,7 @@
 import csv
 import os
 
-# variables we'll need with the file
+# variables we'll need with the election results file
 total_votes = 0
 candidate_options = []
 candidate_votes = {}
@@ -35,48 +35,55 @@ with open(file_to_load) as election_data:
         #running tally of candidate votes
         candidate_votes[candidate_name] += 1
 
-
-#winning candidate variables
-winning_count = 0
-winning_percentage = 0
-winning_candidate = ""
-
-
-#loop through output and determine vote totals and winne
-for candidate in candidate_options:
-    
-    #determine and print each candidates vote total/percentage
-    votes = candidate_votes[candidate]
-    candidate_percent = float(votes)/float(total_votes)*100
-
-    #print all candidate votes
-    print(f'{candidate}: {candidate_percent:.1f}% ({votes:,})\n')
-
-    #determine the winner
-    if (votes > winning_count) and (candidate_percent > winning_percentage):
-        winning_count = votes
-        winning_percentage = candidate_percent
-        winning_candidate = candidate
-
-#print out the winner
-winning_candidate_summary = (
-    f'-------------------------\n'
-    f'Winner: {winning_candidate}\n'
-    f'Winning Vote Count: {winning_count:,}\n'
-    f'Winning Percentage: {winning_percentage:.1f}%\n'
-    f'-------------------------\n'
-)
-
-print(winning_candidate_summary)
-
-
-
 #build path to and create election analysis txt
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 with open(file_to_save, "w") as txt_out:
-    txt_out.write("Counties in the Election\n")
-    txt_out.write("-------------------------\n")
-    txt_out.write("Arapahoe\n")
-    txt_out.write("Denver\n")
-    txt_out.write("Jefferson")
+
+    #print and export total votes
+    election_results = (
+        f'\nElection Results\n'
+        f'-------------------------\n'
+        f'Total Votes: {total_votes:,}\n'
+        f'-------------------------\n'
+    )
+
+    print(election_results, end = "")
+    txt_out.write(election_results)
+
+
+    #winning candidate variables
+    winning_count = 0
+    winning_percentage = 0
+    winning_candidate = ""
+
+
+    #loop through output and determine vote totals and winner
+    for candidate in candidate_options:
+        
+        #determine each candidates vote total/percentage
+        votes = candidate_votes[candidate]
+        candidate_percent = float(votes)/float(total_votes)*100
+
+        #print and export all candidate votes
+        candidate_results = (f'{candidate}: {candidate_percent:.1f}% ({votes:,})\n')
+        print(candidate_results, end = "")
+        txt_out.write(candidate_results)
+
+        #determine the winner
+        if (votes > winning_count) and (candidate_percent > winning_percentage):
+            winning_count = votes
+            winning_percentage = candidate_percent
+            winning_candidate = candidate
+
+    #print and export the winner
+    winning_candidate_summary = (
+        f'-------------------------\n'
+        f'Winner: {winning_candidate}\n'
+        f'Winning Vote Count: {winning_count:,}\n'
+        f'Winning Percentage: {winning_percentage:.1f}%\n'
+        f'-------------------------\n'
+    )
+
+    print(winning_candidate_summary)
+    txt_out.write(winning_candidate_summary)
